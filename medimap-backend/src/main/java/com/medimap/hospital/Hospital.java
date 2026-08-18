0587094cfd28f5fe4cd937ee93e25bd3324716e2
Ion Matei <ionmatei8585@gmail.com>
2026-08-18 10:30:57 +0300

    Finish Hospital JPA

--- medimap-backend/src/main/java/com/medimap/hospital/Hospital.java
+++ medimap-backend/src/main/java/com/medimap/hospital/Hospital.java
@@ -20,4 +20,57 @@ public class Hospital
     private String phone;
 
     private String email;
+    
+    public Hospital() {}
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
 }
