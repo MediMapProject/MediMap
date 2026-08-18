b4d625ae060aa802829f470003bd0a13c8aee80e
Ion Matei <ionmatei8585@gmail.com>
2026-08-18 11:39:28 +0300

    First endpoint

--- medimap-backend/src/main/java/com/medimap/hospital/HospitalController.java
+++ medimap-backend/src/main/java/com/medimap/hospital/HospitalController.java
@@ -1,4 +1,22 @@
 package com.medimap.hospital;
 
-public class HospitalController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HospitalController
+{
+     private HospitalService hospitalService;
+     public HospitalController(HospitalService hospitalService)
+     {
+         this.hospitalService = hospitalService;
+     }
+
+     @GetMapping("/api/hospitals")
+     public List<Hospital> getAllHospitals()
+     {
+         return hospitalService.getAllHospitals();
+     }
 }
