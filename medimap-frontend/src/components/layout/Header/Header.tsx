9c7e1d68e34f88c18e00a8b6fc1633647c9f7b99
Filip Stanciu <filipstanciu2005@gmail.com>
2026-08-19 15:32:18 +0300

    Implement application header

--- medimap-frontend/src/components/layout/Header/Header.tsx
+++ medimap-frontend/src/components/layout/Header/Header.tsx
@@ -1,7 +1,35 @@
+import "./Header.css";
+
 export default function Header() {
     return (
-        <header>
-            Header
+        <header className="header">
+
+            <div className="header__left">
+
+                <h1 className="header__logo">
+                    MediMap
+                </h1>
+
+            </div>
+
+            <div className="header__center">
+
+                <input
+                    className="header__search"
+                    type="text"
+                    placeholder="Search doctor, department or room..."
+                />
+
+            </div>
+
+            <div className="header__right">
+
+                <button>RO</button>
+
+                <button>🌙</button>
+
+            </div>
+
         </header>
     );
 }
