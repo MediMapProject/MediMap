9ca33bef455cddff7c9cad60a9ee6bf6b03bfce4
Filip Stanciu <filipstanciu2005@gmail.com>
2026-08-19 15:34:35 +0300

    Implement application layout

--- medimap-frontend/src/components/layout/Sidebar/Sidebar.tsx
+++ medimap-frontend/src/components/layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,40 @@
+import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
     return (
-        <aside>
-            Sidebar
+        <aside className="sidebar">
+
+            <div className="sidebar__logo">
+
+                <h2>MediMap</h2>
+
+            </div>
+
+            <nav className="sidebar__menu">
+
+                <NavLink to="/map">
+                    Live Map
+                </NavLink>
+
+                <NavLink to="/doctors">
+                    Doctors
+                </NavLink>
+
+                <NavLink to="/departments">
+                    Departments
+                </NavLink>
+
+                <NavLink to="/about">
+                    About
+                </NavLink>
+
+                <NavLink to="/settings">
+                    Settings
+                </NavLink>
+
+            </nav>
+
         </aside>
     );
 }
