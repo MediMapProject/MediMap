f23833ff66782b118fd75f049c5a39ad0d78110c
Ion Matei <ionmatei8585@gmail.com>
2026-08-19 12:59:24 +0300

    Modify BuildingController

--- medimap-backend/src/main/java/com/medimap/building/BuildingController.java
+++ medimap-backend/src/main/java/com/medimap/building/BuildingController.java
@@ -1,4 +1,16 @@
 package com.medimap.building;
 
-public class BuildingController {
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/buildings")
+public class BuildingController
+{
+    private final BuildingService buildingService;
+
+    public BuildingController(BuildingService buildingService) {this.buildingService = buildingService;}
+
+    @GetMapping
+    public List<Building> getAllBuildings() {return buildingService.getAllBuildings();}
 }
--- medimap-backend/src/main/java/com/medimap/building/BuildingService.java
+++ medimap-backend/src/main/java/com/medimap/building/BuildingService.java
@@ -1,8 +1,13 @@
 package com.medimap.building;
+import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BuildingService
 {
     private final BuildingRepository buildingRepository;
 
     public BuildingService(BuildingRepository buildingRepository) {this.buildingRepository = buildingRepository;}
+
+    public List<Building> getAllBuildings() {return buildingRepository.findAll();}
 }
