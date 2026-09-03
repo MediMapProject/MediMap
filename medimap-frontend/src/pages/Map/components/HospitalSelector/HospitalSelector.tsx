6c462cb196377d56529529ff5a37a915b14e42b4
Filip Stanciu <filipstanciu2005@gmail.com>
2026-09-03 15:42:54 +0300

    Change hospitalId type from string to number

--- medimap-frontend/src/pages/Map/components/HospitalSelector/HospitalSelector.tsx
+++ medimap-frontend/src/pages/Map/components/HospitalSelector/HospitalSelector.tsx
@@ -6,8 +6,8 @@ import type { Hospital } from "@/shared/types/Hospital";
 
 type HospitalSelectorProps = {
     hospitals: Hospital[];
-    value: string | null;
-    onChange: (hospitalId: string) => void;
+    value: number | null;
+    onChange: (hospitalId: number) => void;
 };
 
 export default function HospitalSelector({
